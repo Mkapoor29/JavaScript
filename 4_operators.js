b0f183183e2382c528dcd9db3e3909a35ad1e31d
Mkapoor29 <123291579+Mkapoor29@users.noreply.github.com>
2024-03-15 03:10:37 +0530

    Create 4_operators.js

---
+++ 4_operators.js
@@ -0,0 +1,50 @@
+// assignment operator
+
+let a = 4;
+// assign value 4 to a.
+
+// ARITHMATIC OPERATORS
+
+let b = 5;
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a ** b;
+// a raise to power b = Math.pow(a,b)
+let g = a/b;
+let h = a % b;
+let i = a++;
+let j = b--;
+
+
+/*
+ASSIGNMENT OPERATORS
+x = y 
+x += y
+x -= y
+x *= y
+x /= y
+x %= y
+x **= y
+*/
+
+// COMPARISON OPERATORS
+
+let k = 6; 
+let l = 7;
+console.log(k == l); //equal to
+console.log(k === l); //equal to and equal type
+console.log(k != l); //not equal to
+console.log(k !== l); // not equal to or not equal type
+console.log(k>l); // greater than
+console.log(k>=l); // greater than or equal to
+console.log(k<l); // less than
+console.log(k<=l); // less than or equal to
+
+// LOGICAL OPERATORS
+let m = 3 , n = 8;
+console.log( m < 10 && n > 7);
+console.log( m < 2 || n > 7);
+console.log( !(m==4));
+
+//   TERNARY OPERATOR

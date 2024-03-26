6d4d210d72eb2b92fa080f0b36fc906ef2884b4a
Mkapoor29 <123291579+Mkapoor29@users.noreply.github.com>
2024-03-26 05:56:21 +0530

    Create z_randomeConcepts.js

---
+++ z_randomeConcepts.js
@@ -0,0 +1,9 @@
+// ARRAYS ARE OBJECTS
+const arr = [1,2,3,4];
+console.log(typeOf(arr));
+console.log(Array.isArray(arr));
+console.log(arr instanceof Array);
+
+// TOSTRING() CAN CONVERT VALUE OF X OF THE BASE 10 NUMBER SYTEM, TO ITS COUNTERPART IN THE BASE Y NUMBER SYSTEM.
+// (10).toString(2); //1010  convert value of 10 in base10 system into 1010 in base2 system.
+// (10).toString(8); // 12

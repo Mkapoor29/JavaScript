41c173f264162e8ac48a7899e6e9518a800b4fb2
Muskan Kapoor <123291579+Mkapoor29@users.noreply.github.com>
2024-06-19 08:29:21 +0530

    Create 34_testing.js

---
+++ 34_testing.js
@@ -0,0 +1,25 @@
+/*
+Testing might mean making sure that the website looks and behaves as expected. For project manager on a software project testing might mean that a specific piece of software works well with other parts of your system for a software engineer. Testing might mean writing code that doesn't break the existing functionality is bug free and satisfies the requirements as set out in a given task based on what your motivations are.
+*/
+
+// TYPES OF TESTING
+
+// 1) E2E TESTING
+/*
+ In testing tries to imitate how a user might interact with your app. This means that in Italy testing you need to open your web application in a browser and then test it by interacting with the page the same way a user might interact with it. For example, clicking on the log in button are going through the process of adding an item to the shopping cart. In other words, you're testing the entire finished software product from the perspective of the end user. Now here's something you may find interesting. The person testing the app doesn't have to be a developer. Finally, E two E tests are the slowest and take the most time to set up and run. Here are a few examples of eat away testing frameworks available on the market. Web driver Js Protractor and Cyprus.
+*/
+
+// 2) INTEGRATION TESTING
+/*
+ntegration testing is testing how parts of your system interact with other parts of your system. In other words, it's testing how separate parts of your apps work together. Let's explore two examples of integration testing software react testing, library and enzyme integration tests are faster and cheaper than E two E tests but not as fast or as cheap as unit testing.
+*/
+
+// 3)UNIT TESTING
+/*
+Unit testing is the process of testing the smallest units of your source code in isolation. A good example of this is functions. A unit is the smallest piece of code that you can test separately from the rest of the app. Practically the smallest unit of testable code in Js is usually a function or a method. Unit tests are self contained. They're meant to test code in isolation, preferably separate from the rest of your app. This makes unit tests fast to run and easy to write.
+*/
+
+// three level testing pyramid.
+// base is unit testing - highest speed and lowest cost
+// middle is integration testing - average speed and expense
+// top is E2E testing - slowest and most expensive
